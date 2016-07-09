63a030d796695208bb9fe9d487abe73470262a91
Georg Bøe <georg@georgboe.com>
2016-07-10 01:42:45 +0200

    Add uploading to package script.

--- package.sh
+++ package.sh
@@ -3,5 +3,5 @@ set -e
 gox --os=linux
 for i in gofetch*
 do
-[ -x "$i" ] && mv $i gofetch && tar -cJf $i.tar.xz gofetch && rm gofetch
+[ -x "$i" ] && mv $i gofetch && tar -cJf $i.tar.xz gofetch && rm gofetch && s3cmd put --acl-public $i.tar.xz s3://gofetch
 done
